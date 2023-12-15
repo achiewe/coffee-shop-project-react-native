bb3ca7d7c69feff23d5c424cca0fdcd8fa519078
achiewe <mamukashviliachi706@gmail.com>
2023-12-15 19:48:17 +0400

    add new folder assets for png

--- App.tsx
+++ App.tsx
@@ -1,24 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 function App(): React.JSX.Element {
-  return <View></View>;
+  return (
+    <View>
+      <Text> whats up</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});

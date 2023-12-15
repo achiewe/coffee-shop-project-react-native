883558157bea83747a531a993feaf8195f50e177
achiewe <mamukashviliachi706@gmail.com>
2023-12-15 20:33:28 +0400

    add new background image and add it in the component Login

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import {ScrollView, StyleSheet} from 'react-native';
+import Login from './components/Login';
 
 function App(): React.JSX.Element {
-  return <ScrollView style={styles.mainDiv}></ScrollView>;
+  return (
+    <ScrollView style={styles.mainDiv}>
+      <Login />
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
   mainDiv: {
-    width: '100%',
+    flex: 1,
     display: 'flex',
     flexDirection: 'column',
-    minHeight: '100%',
     backgroundColor: '#F9F9F9',
   },
 });
--- components/Login.tsx
+++ components/Login.tsx
@@ -1,9 +1,24 @@
-import {StyleSheet, View} from 'react-native';
+import {ImageBackground, StyleSheet, View} from 'react-native';
 
 export default function Login(): JSX.Element {
-  return <View style={styles.MainCont}> </View>;
+  return (
+    <View style={styles.MainCont}>
+      <ImageBackground
+        source={require('../assets/coffeebg.png')}
+        resizeMode="cover"
+        style={styles.bgImage}></ImageBackground>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  MainCont: {},
+  MainCont: {
+    flex: 1,
+  },
+
+  bgImage: {
+    flex: 1,
+    alignSelf: 'stretch',
+    width: null,
+  },
 });

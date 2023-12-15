b5c40decb5f05aeda815ee0c4a26b158c3f97c25
achiewe <mamukashviliachi706@gmail.com>
2023-12-15 20:07:15 +0400

    add style for View

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,5 +1,9 @@
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 export default function Login(): JSX.Element {
-  return <View> </View>;
+  return <View style={styles.MainCont}> </View>;
 }
+
+const styles = StyleSheet.create({
+  MainCont: {},
+});

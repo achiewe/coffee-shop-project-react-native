855f8d5c9d5b28289476ec51fe3d531d912eced2
achiewe <mamukashviliachi706@gmail.com>
2023-12-17 15:39:39 +0400

    add google button to sign in user

--- components/SignInButton.tsx
+++ components/SignInButton.tsx
@@ -1,7 +1,48 @@
-import {useEffect} from 'react';
+import React, {useEffect} from 'react';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-community/google-signin';
 import {View} from 'react-native';
 
 export default function SignInButton() {
-  useEffect(() => {});
-  return <View></View>;
+  const handleSignIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log('User Info:', userInfo);
+      // Handle successful sign-in
+    } catch (error: any) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // Handle canceled sign-in
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // Handle sign-in in progress
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // Handle Play Services not available
+      } else {
+        // Handle other errors
+        console.error('Google Sign-In Error:', error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    // Initialize GoogleSignin
+    GoogleSignin.configure({
+      webClientId:
+        '796566206974-g5eirele1mff8q149p1qir1u0rd2jkfj.apps.googleusercontent.com', // Replace with your web client ID
+    });
+  }, []);
+
+  return (
+    <View>
+      <GoogleSigninButton
+        style={{width: 192, height: 48}}
+        size={GoogleSigninButton.Size.Standard}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={handleSignIn}
+      />
+    </View>
+  );
 }

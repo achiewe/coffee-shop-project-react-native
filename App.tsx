19099d1799be153cdf8c331b82913a5923249a2f
achiewe <mamukashviliachi706@gmail.com>
2023-12-18 11:59:18 +0400

    add setTimeout in the app.tsx to render first Login and then show MainPage component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import {ScrollView, StyleSheet, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View} from 'react-native';
 import Login from './components/Login';
 import MainPage from './components/MainPage';
 
 function App(): React.JSX.Element {
+  const [showLogin, setShowLogin] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowLogin(false);
+    }, 3000); // Change 3000 to the desired delay in milliseconds
+
+    // Clear the timeout if the component is unmounted before the delay completes
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
-    <View style={styles.mainDiv}>
-      <Login />
-      <MainPage />
-    </View>
+    <View style={styles.mainDiv}>{showLogin ? <Login /> : <MainPage />}</View>
   );
 }
 
--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -1,5 +1,9 @@
-import {View} from 'react-native';
+import {Text, View} from 'react-native';
 
 export default function MainPage() {
-  return <View> </View>;
+  return (
+    <View>
+      <Text> hello</Text>
+    </View>
+  );
 }

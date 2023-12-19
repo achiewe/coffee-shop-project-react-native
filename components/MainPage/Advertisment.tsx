62d88156781caee9cb1897b954f27b002776384e
achiewe <mamukashviliachi706@gmail.com>
2023-12-19 19:28:02 +0400

    create advertisment component and write there backgrounImage with style

--- components/MainPage/Advertisment.tsx
+++ components/MainPage/Advertisment.tsx
@@ -2,16 +2,27 @@ import {ImageBackground, StyleSheet, View} from 'react-native';
 
 export default function Advertisment() {
   return (
-    <ImageBackground
-      source={require('../../assets/coffeeImg.png')}
-      resizeMode="contain"
-      style={styles.coffeeImg}></ImageBackground>
+    <View style={styles.viewImg}>
+      <ImageBackground
+        source={require('../../assets/coffeeImg.png')}
+        resizeMode="contain"
+        imageStyle={{borderRadius: 10}}
+        style={styles.coffeeImg}></ImageBackground>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  viewImg: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
   coffeeImg: {
     width: 315,
     height: 140,
+    marginTop: -60,
   },
 });

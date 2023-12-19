113d481003eab561d3c6aea5fe6826bbb1af27fd
achiewe <mamukashviliachi706@gmail.com>
2023-12-19 19:13:24 +0400

    style the imageBacgkround div

--- components/MainPage/Advertisment.tsx
+++ components/MainPage/Advertisment.tsx
@@ -1,9 +1,17 @@
-import {ImageBackground, View} from 'react-native';
+import {ImageBackground, StyleSheet, View} from 'react-native';
 
 export default function Advertisment() {
   return (
-    <ImageBackground source={require('../../assets/coffeeImg.png')}>
-      {' '}
-    </ImageBackground>
+    <ImageBackground
+      source={require('../../assets/coffeeImg.png')}
+      resizeMode="contain"
+      style={styles.coffeeImg}></ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  coffeeImg: {
+    width: 315,
+    height: 140,
+  },
+});

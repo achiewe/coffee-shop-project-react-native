df67c12e8884332705eb39e3ec9d94558061a768
achiewe <mamukashviliachi706@gmail.com>
2023-12-22 16:55:47 +0400

    add map method in the component for map the basic data

--- components/MainPage/ListCoffeeTitle.tsx
+++ components/MainPage/ListCoffeeTitle.tsx
@@ -1,10 +1,12 @@
-import {ScrollView} from 'react-native';
+import {ScrollView, TouchableWithoutFeedback, View} from 'react-native';
 import data from '../../data.json';
 
 export default function ListCoffeeTitle() {
   return (
-    <ScrollView
-      horizontal={true}
-      showsHorizontalScrollIndicator={false}></ScrollView>
+    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+      {data.coffee_categories.map(item => {
+        return <View key={item.id}></View>;
+      })}
+    </ScrollView>
   );
 }

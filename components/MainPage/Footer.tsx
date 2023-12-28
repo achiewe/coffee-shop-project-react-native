0bf001c8ae6eb8742d7998f37799a26b183fd0cd
achiewe <mamukashviliachi706@gmail.com>
2023-12-28 14:58:59 +0400

    style the footer component

--- components/MainPage/Footer.tsx
+++ components/MainPage/Footer.tsx
@@ -1,5 +1,22 @@
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 export default function Footer() {
-  return <View></View>;
+  return <View style={styles.footerWarp}></View>;
 }
+
+const styles = StyleSheet.create({
+  footerWarp: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'flex-end',
+    width: '100%',
+    height: 50,
+    backgroundColor: 'red',
+    borderWidth: 1,
+    borderColor: '#F1F1F1',
+    borderRadius: 20,
+    gap: 59,
+  },
+});

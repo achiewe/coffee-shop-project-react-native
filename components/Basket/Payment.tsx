abf2e262da54b5406cac8f687358e930ffe2c437
achiewe <mamukashviliachi706@gmail.com>
2024-01-04 13:34:06 +0400

    add the price and discout View and style it

--- components/Basket/Payment.tsx
+++ components/Basket/Payment.tsx
@@ -1,7 +1,18 @@
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function Payment() {
-  return <View> </View>;
+  return (
+    <View style={styles.containetPayment}>
+      <Text style={styles.paymentText}> Payment Summary</Text>
+      <View style={styles.priceContainer}>
+        <View style={styles.priceDeliveryView}>
+          <View style={styles.priceView}>
+            <Text style={styles.priceTitle}> Price</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -10,7 +21,44 @@ const styles = StyleSheet.create({
     width: '100%',
     flexDirection: 'column',
     gap: 9,
-    paddingLeft: 26,
-    paddingRight: 26,
+    paddingLeft: 36,
+    paddingRight: 36,
+    marginTop: 21,
+  },
+
+  paymentText: {
+    color: '#2F2D2C',
+    fontSize: 16,
+    fontStyle: 'normal',
+    fontWeight: '600',
+  },
+
+  priceContainer: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 21,
+  },
+
+  priceDeliveryView: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 14,
+  },
+
+  priceView: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  priceTitle: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: '#2F2D2C',
+    fontStyle: 'normal',
   },
 });

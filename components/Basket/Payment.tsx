e2f1f49fb96a8e3506ee8003903ecd42643f85f0
achiewe <mamukashviliachi706@gmail.com>
2024-01-04 13:21:26 +0400

    add the style fr the main contaier of the payment

--- components/Basket/Payment.tsx
+++ components/Basket/Payment.tsx
@@ -1,5 +1,16 @@
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 export default function Payment() {
   return <View> </View>;
 }
+
+const styles = StyleSheet.create({
+  containetPayment: {
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'column',
+    gap: 9,
+    paddingLeft: 26,
+    paddingRight: 26,
+  },
+});

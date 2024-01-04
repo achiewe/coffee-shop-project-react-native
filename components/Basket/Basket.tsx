f7501d26d77c06c1225970d6fa4bb7ce2e8d9adc
achiewe <mamukashviliachi706@gmail.com>
2024-01-04 11:49:17 +0400

    add the styles in the basket component

--- components/Basket/Basket.tsx
+++ components/Basket/Basket.tsx
@@ -1,10 +1,18 @@
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 // component for the basket component
 export default function Basket() {
   return (
-    <View>
+    <View style={styles.mainContainer}>
       <Text> whats up guys</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'column',
+  },
+});

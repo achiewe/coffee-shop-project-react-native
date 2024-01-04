619ede8df4c98199719a490c011e34034daa8e84
achiewe <mamukashviliachi706@gmail.com>
2024-01-04 13:12:00 +0400

    create the payment component for the basket

--- components/Basket/Basket.tsx
+++ components/Basket/Basket.tsx
@@ -1,6 +1,7 @@
 import {StyleSheet, View} from 'react-native';
 import BasketHeadline from './BasketHeadline';
 import ItemDiscount from './ItemDiscount';
+import Payment from './Payment';
 
 // component for the basket component
 export default function Basket(): React.JSX.Element {
@@ -8,6 +9,7 @@ export default function Basket(): React.JSX.Element {
     <View style={styles.mainContainer}>
       <BasketHeadline />
       <ItemDiscount />
+      <Payment />
     </View>
   );
 }

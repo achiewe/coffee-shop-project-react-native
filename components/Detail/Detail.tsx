fcd0e8ce8d5f962d6b4f1dd061c431a4560c9b53
achiewe <mamukashviliachi706@gmail.com>
2024-01-05 13:48:57 +0400

    remove the Text from the component

--- components/Detail/Detail.tsx
+++ components/Detail/Detail.tsx
@@ -1,9 +1,14 @@
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function Detail() {
-  return (
-    <View>
-      <Text> asdasdd</Text>
-    </View>
-  );
+  return <View style={styles.mainDetail}></View>;
 }
+
+const styles = StyleSheet.create({
+  mainDetail: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: '#F9F9F9',
+  },
+});

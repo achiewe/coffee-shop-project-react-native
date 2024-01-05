80763fb7158e6bf0c2a8df8d592ffd7973fd3f1d
achiewe <mamukashviliachi706@gmail.com>
2024-01-05 18:09:31 +0400

    add the style for paragrhaph

--- components/Detail/Description.tsx
+++ components/Detail/Description.tsx
@@ -1,9 +1,16 @@
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function Description() {
   return (
     <View style={styles.mainDesc}>
       <View style={styles.line} />
+      <View style={styles.titleDescView}>
+        <Text style={styles.title}> Description</Text>
+        <Text style={styles.paragraph}>
+          A cappuccino is an approximately 150 ml beverage, with 25 ml of
+          espresso coffee and 85ml of fresh milk the fo.. Read More
+        </Text>
+      </View>
     </View>
   );
 }
@@ -14,14 +21,38 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'column',
     gap: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
     marginTop: 28,
+    paddingLeft: 31,
+    paddingRight: 31,
   },
 
   line: {
     height: 1,
-    width: 315,
+    width: '100%',
     backgroundColor: '#EAEAEA',
+  },
+
+  titleDescView: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    gap: 15,
+  },
+
+  title: {
+    color: '#2F2D2C',
+    fontSize: 16,
+    fontWeight: '600',
+    fontStyle: 'normal',
+  },
+
+  paragraph: {
+    fontSize: 14,
+    color: '#9B9B9B',
+    fontWeight: '400',
+    fontStyle: 'normal',
   },
 });

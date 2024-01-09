5b27dcdff0392f03ae046307ad187f8ca3fa9bc7
achiewe <mamukashviliachi706@gmail.com>
2024-01-09 18:58:02 +0400

    add the comment in the store to use state in the project

--- features/store.ts
+++ features/store.ts
@@ -1,6 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit';
 import TitleCategoryId, {TitleCatProps} from './TitleCategoryId';
 
+// add the state in the store
 const store = configureStore({
   reducer: {
     id: TitleCategoryId,

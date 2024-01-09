23b4579033178b3ff4ea82befd6b20103fb4d244
achiewe <mamukashviliachi706@gmail.com>
2024-01-09 21:46:46 +0400

    style the text price text view and text

--- components/Detail/BuyNow.tsx
+++ components/Detail/BuyNow.tsx
@@ -3,7 +3,10 @@ import {StyleSheet, Text, View} from 'react-native';
 export default function BuyNow() {
   return (
     <View style={styles.mainView}>
-      <Text> hello</Text>
+      <View style={styles.viewPrice}>
+        <Text style={styles.textPrice}>Price </Text>
+        <Text> </Text>
+      </View>
     </View>
   );
 }
@@ -11,7 +14,29 @@ export default function BuyNow() {
 const styles = StyleSheet.create({
   mainView: {
     width: '100%',
-    height: 1,
-    backgroundColor: '#EAEAEA',
+    height: '100%',
+    borderTopRightRadius: 20,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 23,
+    paddingHorizontal: 18,
+    borderTopLeftRadius: 20,
+    borderColor: '#EAEAEA',
+    borderWidth: 1,
+  },
+
+  viewPrice: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 8,
+  },
+
+  textPrice: {
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    color: '#9B9B9B',
   },
 });

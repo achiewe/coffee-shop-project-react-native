8b37c92f4e5ce34f7e562b03bbbd39d45bfcce58
achiewe <mamukashviliachi706@gmail.com>
2024-01-12 17:41:58 +0400

    add the state in the store file to use it in the project

--- features/store.ts
+++ features/store.ts
@@ -1,12 +1,14 @@
 import {configureStore} from '@reduxjs/toolkit';
 import TitleCategoryIdSlice, {TitleCatProps} from './TitleCategoryIdSlice';
 import AddBasketSlice, {BasketProps} from './AddBasketSlice';
+import getInputValueSlice, {inputProps} from './getInputValueSlice';
 
 // add the state in the store
 const store = configureStore({
   reducer: {
     id: TitleCategoryIdSlice,
     addCard: AddBasketSlice,
+    inputValue: getInputValueSlice,
   },
 });
 
@@ -14,6 +16,7 @@ const store = configureStore({
 export type RootState = {
   id: TitleCatProps;
   addCard: BasketProps;
+  inputValue: inputProps;
 };
 
 export default store;

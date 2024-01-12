579998e4b8e4ba59bf6ab7c9de668999ba7db2f9
achiewe <mamukashviliachi706@gmail.com>
2024-01-12 13:04:16 +0400

    add the state in the store file and write that state type

--- features/store.ts
+++ features/store.ts
@@ -1,16 +1,19 @@
 import {configureStore} from '@reduxjs/toolkit';
 import TitleCategoryId, {TitleCatProps} from './TitleCategoryId';
+import AddBasket, {BasketProps} from './AddBasket';
 
 // add the state in the store
 const store = configureStore({
   reducer: {
     id: TitleCategoryId,
+    addCard: AddBasket,
   },
 });
 
 // add the state in the RootState
 export type RootState = {
   id: TitleCatProps;
+  addCard: BasketProps;
 };
 
 export default store;

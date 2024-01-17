9da40bef9153ff6b10f27dfb997af64b4997abae
achiewe <mamukashviliachi706@gmail.com>
2024-01-17 16:14:17 +0400

    add on the onpress setItemCount to grow it when will onpress plus 1

--- components/MainPage/ItemList.tsx
+++ components/MainPage/ItemList.tsx
@@ -60,12 +60,6 @@ export default function ItemList() {
     }
   }, [addBasket, itemId]);
 
-  const product = useSelector(
-    (store: RootState) => store.AddProduct.AddProduct,
-  );
-
-  console.log(product);
-
   return (
     <View style={styles.mainContainer}>
       {filteredItems.map(item => (

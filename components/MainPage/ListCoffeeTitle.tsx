1319d52efd99465192548617fd5018a155e78ec6
achiewe <mamukashviliachi706@gmail.com>
2024-01-17 15:19:26 +0400

    add the backgroundColor logic in the component

--- components/MainPage/ListCoffeeTitle.tsx
+++ components/MainPage/ListCoffeeTitle.tsx
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import {
   ScrollView,
   StyleSheet,
@@ -9,12 +10,13 @@ import data from '../../data.json';
 import {useDispatch} from 'react-redux';
 import {setId} from '../../features/TitleCategoryIdSlice';
 
-// list of the data
 export default function ListCoffeeTitle() {
   const dispatch = useDispatch();
+  const [selectedId, setSelectedId] = useState<number | null>(null);
 
   const getId = (id: number): void => {
     dispatch(setId(id));
+    setSelectedId(id);
   };
 
   return (
@@ -23,13 +25,20 @@ export default function ListCoffeeTitle() {
       showsHorizontalScrollIndicator={false}
       style={style.MainContainer}>
       {data.coffee_categories.map(item => {
+        const isSelected = selectedId === item.id;
+
         return (
           <TouchableWithoutFeedback
             key={item.id}
             onPress={() => {
               getId(item.id);
             }}>
-            <View key={item.id} style={style.TitleView}>
+            <View
+              key={item.id}
+              style={[
+                style.TitleView,
+                {backgroundColor: isSelected ? '#C67C4E' : '#F3F3F3'},
+              ]}>
               <Text style={style.titleText}>{item.category}</Text>
             </View>
           </TouchableWithoutFeedback>
@@ -39,7 +48,6 @@ export default function ListCoffeeTitle() {
   );
 }
 
-// style for the ListCoffeeTitle
 const style = StyleSheet.create({
   MainContainer: {
     width: '93%',
@@ -52,7 +60,6 @@ const style = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginHorizontal: 10,
-    backgroundColor: '#F3F3F3',
     height: 38,
     padding: 10,
     borderRadius: 12,

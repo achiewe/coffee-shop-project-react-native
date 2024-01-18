80780c3f756137ee52fae4a8f415777cb9cc5a6f
achiewe <mamukashviliachi706@gmail.com>
2024-01-18 11:45:02 +0400

    add the useRoute on the MainPage component

--- components/MainPage/MainPage.tsx
+++ components/MainPage/MainPage.tsx
@@ -3,9 +3,15 @@ import InputDiv from './InputDiv';
 import Advertisment from './Advertisment';
 import ListCoffeeTitle from './ListCoffeeTitle';
 import ItemList from './ItemList';
+import {useRoute} from '@react-navigation/native';
 
 // mainPage function
 export default function MainPage() {
+  const route = useRoute();
+  const name = route.name;
+
+  console.log(name);
+
   return (
     <ScrollView style={styles.mainDiv}>
       <InputDiv />

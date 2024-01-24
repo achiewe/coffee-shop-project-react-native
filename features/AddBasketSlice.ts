891a5aaae2cbe1460e064aa127fe32007b821ef2
achiewe <mamukashviliachi706@gmail.com>
2024-01-24 14:35:47 +0400

    add the comment in the file

--- features/AddBasketSlice.ts
+++ features/AddBasketSlice.ts
@@ -1,13 +1,16 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
+// define the interface for the BasketProps
 export interface BasketProps {
   addCard: boolean;
 }
 
+// define the initial state for the basket, with addCard set to false
 const initialState: BasketProps = {
   addCard: false,
 };
 
+// Create a Redux slice using createSlice
 const AddBasketSlice = createSlice({
   name: 'addCard',
   initialState,
@@ -18,5 +21,8 @@ const AddBasketSlice = createSlice({
   },
 });
 
+// extract the action creator from the slice
 export const {setAddCard} = AddBasketSlice.actions;
+
+// export the reducer function to be used in the Redux store
 export default AddBasketSlice.reducer;
